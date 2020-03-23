ac1f2d33a8464bac17848e8a8f9497add1709dd6
Renato Pavlić <renato.poslovni@gmail.com>
2020-03-23 19:26:07 +0100

    sample navbar and welcome div

---
+++ frontend/src/components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import style from "./navbar.css"
+
+function Navbar() {
+    return (
+        <div className="mynavbar">
+
+            <button className="sing-in">Sing in</button>
+            <button className="register">Register</button>
+            
+        </div>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/components/Welcome/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from "./welcome.css"
+
+function Welcome() {
+    return (
+        <div className="welcomediv">
+            <img className="mylogo" src="../../images/logo.png"></img>
+        </div>
+    )
+}
+
+export default Welcome
