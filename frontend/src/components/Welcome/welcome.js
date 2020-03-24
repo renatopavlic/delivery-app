798ef828b2b618f94cbb378c610bedf0596965fc
Renato Pavlić <renato.poslovni@gmail.com>
2020-03-24 21:33:26 +0100

    Dodao par stvari

--- frontend/src/components/Welcome/welcome.js
+++ frontend/src/components/Welcome/welcome.js
@@ -5,9 +5,34 @@ import style from "./welcome.css"
 function Welcome() {
     return (
         <div className="welcomediv">
-            <img className="mylogo" src="/images/logo.png"></img>
-            <p className="welcomep">Naruči što želiš, dostavljamo ti za nekoliko minuta!</p>
+                <img className="mylogo" src="/images/logo.png"></img>
+                <p className="welcomep">Naruči što želiš, dostavljamo ti za nekoliko minuta!</p>
+
+            <div className="myrow">
+
+               <button className="list_btn">
+                   <img src="" className="list_img"></img>
+                   <h4 className="list_text">Neki tekst</h4>
+               </button>
+
+               <button className="list_btn">
+                   <img src="" className="list_img"></img>
+                   <h4 className="list_text">Neki tekst</h4>
+               </button>
+
+               <button className="list_btn">
+                   <img src="" className="list_img"></img>
+                   <h4 className="list_text">Neki tekst</h4>
+               </button>
+
+               <button className="list_btn">
+                   <img src="" className="list_img"></img>
+                   <h4 className="list_text">Neki tekst</h4>
+               </button>
+            </div>
+
         </div>
+
     )
 }
 
